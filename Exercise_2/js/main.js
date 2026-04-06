38576e4a930ae4ad1f70050c1017e49ab445cb55
Valkary <pepoclesng@gmail.com>
2026-04-06 17:35:50 -0600

    Exercise 2

---
+++ Exercise_2/js/main.js
@@ -0,0 +1,12 @@
+let data = [25, 20, 15, 10, 5];
+
+const svg = d3.select("#chart-area").append("svg")
+  .attr("width", 400)
+  .attr("height", 400);
+
+data.forEach((height, i) => svg.append("rect")
+  .attr("height", height)
+  .attr("width", 40)
+  .attr("y", 10)
+  .attr("x", i * 45 + 5)
+);
